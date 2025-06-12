035703f85819e91c6e57d093f1c44e08c3e89cb8
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2025-06-12 17:32:53 +0300

    *

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,9 +1,11 @@
-﻿using Movies.Application;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Movies.Application;
 using Movies.Application.Database;
 using Scalar.AspNetCore;
 
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
 var config = builder.Configuration;
 
 
@@ -18,7 +20,14 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+
+    app.MapScalarApiReference(options => {
+        options.Title = "Zm.Movies";
+        //options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        options.ShowSidebar = true;
+
+        options.Servers = Array.Empty<ScalarServer>();
+    });
 }
 
 app.UseHttpsRedirection();
