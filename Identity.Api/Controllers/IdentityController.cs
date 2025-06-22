d4ca5744b920cf0cce485aae45b7ee303833d39a
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2025-06-22 12:28:47 +0300

    *

---
+++ Identity.Api/Controllers/IdentityController.cs
@@ -0,0 +1,55 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity.Api.Controllers;
+
+[ApiController]
+public class IdentityController : ControllerBase {
+    private const string TokenSecret = "ForTheLoveOfGodStoreAndLoadThisSecurely";
+    private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(8);
+
+    [HttpPost("token")]
+    public IActionResult GenerateToken(
+        [FromBody] TokenGenerationRequest request) {
+        JwtSecurityTokenHandler tokenHandler = new ();
+        byte[] key = Encoding.UTF8.GetBytes(TokenSecret);
+
+        List<Claim> claims = new() {
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Sub, request.Email),
+            new(JwtRegisteredClaimNames.Email, request.Email),
+            new("userid", request.UserId.ToString())
+        };
+
+        foreach(KeyValuePair<string, object> claimPair in request.CustomClaims) {
+            JsonElement jsonElement = (JsonElement)claimPair.Value;
+            string valueType = jsonElement.ValueKind switch
+            {
+                JsonValueKind.True => ClaimValueTypes.Boolean,
+                JsonValueKind.False => ClaimValueTypes.Boolean,
+                JsonValueKind.Number => ClaimValueTypes.Double,
+                _ => ClaimValueTypes.String
+            };
+
+            Claim claim = new(claimPair.Key, claimPair.Value.ToString()!, valueType);
+            claims.Add(claim);
+        }
+
+        SecurityTokenDescriptor tokenDescriptor = new() {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.Add(TokenLifetime),
+            Issuer = "https://id.nickchapsas.com",
+            Audience = "https://movies.nickchapsas.com",
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+        string jwt = tokenHandler.WriteToken(token);
+        return Ok(jwt);
+    }
+}
---
+++ Identity.Api/Program.cs
@@ -0,0 +1,20 @@
+﻿using Scalar.AspNetCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+WebApplication app = builder.Build();
+if(app.Environment.IsDevelopment()) {
+    app.MapOpenApi();
+    app.MapScalarApiReference(options => {
+        options.Title = "Zm.Movies";
+        //options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        options.ShowSidebar = true;
+
+        options.Servers = Array.Empty<ScalarServer>();
+    });
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
